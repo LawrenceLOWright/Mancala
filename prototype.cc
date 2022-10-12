34aeb71496148212b671460a2896ace29a091d89
Lawrence Wright <LWright@oberlin.edu>
2022-10-12 23:14:17 +0000

    Initial commit

---
+++ prototype.cc
@@ -0,0 +1,43 @@
+//A base for our prototype
+
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+
+//initializes end, a variable that will tell us if the game is over or if it has ended
+int end = 0;
+
+//initalizes variables to hold computer (c) and player (p) # of points
+int cgoal = 0;
+int pgoal = 0;
+
+//initalizes the stones in each hole for player and computer
+int p1 = 4;
+int p2 = 4;
+int p3 = 4;
+int p4 = 4;
+int p5 = 4;
+int p6 = 4;
+
+int c1 = 4;
+int c2 = 4;
+int c3 = 4;
+int c4 = 4;
+int c5 = 4;
+int c6 = 4;
+
+while (end == 0) {
+  //player moves
+
+  //if game is over, break
+  
+  //computer checks if it can get an extra turn
+
+  //computer checks if it can steal through empty space
+
+  //if not extra turn or steal, computer picks random hole and moves
+
+  //if game is over, break
+ }
+
+//print final scores
