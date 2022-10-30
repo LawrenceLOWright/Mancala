f69e6accd3077f85eaffa543e6911f91e5642f17
Lawrence Wright <LWright@oberlin.edu>
2022-10-30 00:02:05 +0000

    Fixed bugs!

--- prototype.cc
+++ prototype.cc
@@ -8,18 +8,9 @@
 int end = 0;
 
 //initalizes the stones in each hole for player and computer                  
-//0-5 are holes, 6 is the end hole                                               //initalize the array to hold tokens and score
-  int score_array[14];
+//0-5 are holes, 6 is the end hole                                             //initalize the array to hold tokens and score
+int score_array[14];
  
-  for (int i = 0; i < 15; i++) {
-    if (i == 6 || i == 13) {
-      score_array[i] = 0;
-    }
-    else {
-      score_array[i] = 4;
-    }
-  }
-
 void next_turn(int position){
   int tokens = score_array[position];
   int turn = -1;
@@ -116,45 +107,67 @@ void next_turn(int position){
   }
   } 
   else{
-    if(turn==1){
+    if(turn == 1){
       turn = 0;
       //Player turn
     }
-    if(turn==0){
+    if(turn == 0){
       turn = 1;
       //Computer turn
     }
   }
+  }
+  }
 }
 
-
-while (end == 0) {
-  //player moves
-
-  //if game is over, break
+  int main(void) {
+    for (int i = 0; i < 15; i++) {
+      if (i == 6 || i == 13) {
+        score_array[i] = 0;
+      }
+      else {
+        score_array[i] = 4;
+      }
+    }
   
-  //computer checks if it can get an extra turn
-  for(int i=0;i<6;i++){
-    if(c[i] == i){
-      for(int n=i;n>=0;n++){
-	c[n] = c[n] + 1;
+    while (end == 0) {
+      //player moves BAD FIX IT
+      for (int i = 0; i < 14; i++) {
+	printf("%s", "Player side: ");
+	if (i < 5) {
+	  printf("%s%d%s", " ", score_array[i], " ");
+	printf("/n%s", "Computer side: ");
+	if (i > 6) {
+          printf("%s%d%s", " ", score_array[i], " ");
+        }
+
       }
-      next = 1;
-      break;
-    }
-  }
-  if(next==1){
-    continue;
-  }
-  //computer checks if it can steal through empty space
-  for(int i=0;i<5;i++){
-    for(int n=6;n>i;n--){
-      if(c[i]==0&&c[n]==(n-i)){
-	next_turn(n);
+      printf("%s/n", "Pick a number from 1 to 6: ");
+      int player_input = -1;
+      player_input = getchar();
+      player_input = (int) player_input;
+      next_turn(player_input);
+
+      //if game is over, break
+  
+      //computer checks if it can get an extra turn
+      for (int i = 7; i < 13; i++){
+	if (score_array[i] == 13-i){
+	  next_turn(i);
+	}
       }
-    }
-  }
-		
+      //computer checks if it can steal through empty space
+      for (int i = 8; i < 13; i++){
+	if (score_array[i] == 0){
+	  for(int n = 7; n < i; n++){
+	    if(score_array[n] == (i - n)){
+	      next_turn(n);
+	  }
+	  }
+	}
+      }
+    
+  
   //if not extra turn or steal, computer picks random hole and moves
   int r = 1 + rand()%6;
   next_turn(r);
@@ -162,16 +175,18 @@ while (end == 0) {
   if(end==1){
     break;
   }
- }
+    }
+  
 
 //print final scores
-printf("%s%d%s%d","Player Score: ",pgoal," Computer Score: ",cgoal);
-if(pgoal>cgoal){
-  printf("%s","Player wins!");
- }
-if(cgoal>pgoal){
-  printf("%s","Computer wins!");
- }
-if(cgoal==pgoal){
-  printf("%s","It's a tie!");
- }
+    printf("%s%d%s%d","Player Score: ",score_array[6]," Computer Score: ",score_array[13]);
+    if(score_array[6] > score_array[13]){
+      printf("%s", "Player wins!");
+    }
+    if(score_array[6] < score_array[13]){
+      printf("%s", "Computer wins!");
+    }
+    if(score_array[6] == score_array[13]){
+      printf("%s","It's a tie!");
+    }
+  }
